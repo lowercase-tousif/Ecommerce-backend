70fd6bf3c5a0536f93e72921d1c92f2a6ae645f1
inverse-tousif <toisiftasrik@gmail.com>
2024-08-10 01:04:25 +0600

    Middleware added self made used. For better request response used morgan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const morgan = require("morgan");
+
+// Self made middlewares
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 7,
+  message: "<h1>Too many requests from this IP. Please try again later</h1>",
+});
+
+// Using middlewares
+app.use(morgan("dev"));
+app.use(rateLimiter);
+app.use(xssClean());
 
 // basic route
 app.get("/", (req, res, next) => {
