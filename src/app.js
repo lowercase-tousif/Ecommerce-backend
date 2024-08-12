67df2db5269a73597cf0c9b13502ad10dcd97cdd
inverse-tousif <toisiftasrik@gmail.com>
2024-08-12 22:26:18 +0600

    Client error and server error handled

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const xssClean = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const createError = require("http-errors");
 // All routes imports here
 const userRoute = require("./routes/userRoute");
 
@@ -27,6 +28,19 @@ app.use("/api/user", userRoute);
 // basic route
 app.get("/", (req, res, next) => {
   res.send("<h1>Server is running</h1>");
+});
+
+// client Error handling
+app.use((req, res, next) => {
+  next(createError(404, "Route not found"));
+});
+
+// server error handling
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
 });
 
 module.exports = app;
