79d9560213cfd8523d2a77dd0d7987ba1b12d0dc
inverse-tousif <toisiftasrik@gmail.com>
2024-08-12 22:27:18 +0600

    Dummy data added

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const data = {
+  users: [
+    {
+      name: "Tousif Tasrik",
+      email: "tousifabc@gmail.com",
+      password: "abcdefg",
+      phone: "0191555",
+      address: "Mohammadpur Dhaka",
+    },
+    {
+      name: "Sidratul",
+      email: "sidratul567@gmail.com",
+      password: "abcdefgh",
+      phone: "017433333",
+      address: "Dhanmondi Dhaka",
+    },
+  ],
+};
+
+module.exports = data;
