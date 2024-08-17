fdd337c44ce78fc26e05479fc03c1b8a79e524de
lowercase-tousif <toisiftasrik@gmail.com>
2024-08-17 11:58:13 +0600

    Finding users by regexp and filtering users to get the users

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,7 +4,25 @@ const router = express.Router();
 
 const getUsers = async (req, res, next) => {
   try {
-    const users = await userModel.find();
+    // Pagination and Search
+    const search = req.query.search || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+
+    // Regular Expression to get users
+    const searchRegularExpression = new RegExp(".*" + search + ".*", "i");
+
+    // Filter
+    const filter = {
+      isAdmin: { $ne: true },
+      $or: [
+        { name: { $regex: searchRegularExpression } },
+        { email: { $regex: searchRegularExpression } },
+        { phone: { $regex: searchRegularExpression } },
+      ],
+    };
+
+    const users = await userModel.find(filter);
     res.status(200).json({
       message: "User fetched",
       users: users,
@@ -12,9 +30,6 @@ const getUsers = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-
-
-
 };
 
 module.exports = getUsers;
